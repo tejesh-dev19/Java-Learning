048b99ac0b3b1062699842a20ae75bfe3157d2ad
Tejesh Chintala <chintalatejesh2005@gmail.com>
2026-09-01 19:57:07 +0530

    add Encapsulation example

---
+++ oops/Encapsulation.java
@@ -0,0 +1,49 @@
+package oops;
+
+class Student {
+    private int id;
+    private String name;
+    private String branch;
+
+    //setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    //getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+}
+
+
+public class Encapsulation {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setId(12);
+        s1.setName("James");
+        s1.setBranch("cse");
+        System.out.println(s1.getId());
+        System.out.println(s1.getName());
+        System.out.println(s1.getBranch());
+
+    }
+}
+
