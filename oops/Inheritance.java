e5dd539f8443d9f952aafae7dd256cc82cb0711a
Tejesh Chintala <chintalatejesh2005@gmail.com>
2026-09-04 17:49:52 +0530

    add inheritance example

--- oops/Inheritance.java
+++ oops/Inheritance.java
@@ -1,17 +1,24 @@
 package oops;
 
 class A2{ //parent class or super class or base class
+    public A2(){
+        System.out.println("in A2 parent class constructor");
+    }
+
     void display(){
         System.out.println("in A class display method");
     }
+    
 }
 
 class B extends A2{//child class or derived class or sub class
-
+    public B(){
+        super();//super method calls the parent class constructor
+    }
 }
 
 class C extends B{
-    
+   
 }
 
 
