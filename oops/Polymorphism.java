2403786cb6ea03a2cb6e21c87ad34dad37a9c513
Tejesh Chintala <chintalatejesh2005@gmail.com>
2026-09-05 15:42:27 +0530

    add polymorphism example

---
+++ oops/Polymorphism.java
@@ -0,0 +1,72 @@
+package oops;
+class Animal{
+    public void run(){
+        System.out.println("Animal do running");
+    }
+    public void eat(){
+        System.out.println("Animal eat food");
+    }
+    public void jump(){
+        System.out.println("animal do jumping");
+    }
+}
+
+class Tiger extends Animal{
+    public void run(){
+        System.out.println("Tiger do running");
+    }
+    public void eat(){
+        System.out.println("Tiger eat food");
+    }
+    public void jump(){
+        System.out.println("Tiger  do jumping");
+    }
+}
+
+
+class Monkey extends Animal{
+    public void run(){
+        System.out.println("monkey do running");
+    }
+    public void eat(){
+        System.out.println("Monkey eat food");
+    }
+    public void jump(){
+        System.out.println("monkey jump on the trees");
+    }
+}
+
+class Forest{
+    public void permit(Animal animal){
+        animal.eat();
+        animal.run();
+        animal.jump();
+    } 
+}
+
+public class Polymorphism {
+    public static void main(String[] args) {
+
+        Tiger t= new Tiger();
+
+        Monkey m = new Monkey();
+
+        // Animal animal;
+        // animal=t;
+        // animal.eat();
+        // animal.run();
+        // System.out.println("************");
+
+        // animal=m;
+        // animal.eat();
+        // animal.run();
+        // ((Monkey) animal).jump();
+
+
+        Forest f = new Forest();
+        f.permit(t);
+        f.permit(m);
+        
+    }
+    
+}
