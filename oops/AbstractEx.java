1680066991c00fc9aea3d1ef0fcb33ee2dc49b47
Tejesh Chintala <chintalatejesh2005@gmail.com>
2026-09-05 16:06:45 +0530

    add Abstract example

---
+++ oops/AbstractEx.java
@@ -0,0 +1,20 @@
+package oops;
+abstract class A3{
+    abstract public void display();
+    public void display2(){
+        System.out.println("In a class display 2 method");
+    }
+}
+
+class B2 extends A3{
+    public void display(){
+        System.out.println("in B2 display method");
+    }
+} 
+public class AbstractEx {
+    public static void main(String[] args) {
+        B2 b = new B2();
+        b.display2();
+    }
+    
+}
