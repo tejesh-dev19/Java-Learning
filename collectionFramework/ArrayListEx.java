3e916af048c4556b7105534966051fdd70807c12
Tejesh Chintala <chintalatejesh2005@gmail.com>
2026-09-06 23:09:34 +0530

    add ArrayList example

---
+++ collectionFramework/ArrayListEx.java
@@ -0,0 +1,18 @@
+package collectionFramework;
+
+import java.util.ArrayList;
+
+public class ArrayListEx {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        al.add(66);
+        al.add(0,58);
+        al.add(99);
+
+        System.out.println(al);
+    }
+    
+}
