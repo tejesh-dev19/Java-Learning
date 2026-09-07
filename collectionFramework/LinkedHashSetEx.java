469d82bb29c749ef0b6c81fcf6f1a199f8c49a63
Tejesh Chintala <chintalatejesh2005@gmail.com>
2026-09-07 19:22:17 +0530

    add LinkedHashSet example

---
+++ collectionFramework/LinkedHashSetEx.java
@@ -0,0 +1,17 @@
+package collectionFramework;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetEx {
+    public static void main(String[] args) {
+        LinkedHashSet lhs = new LinkedHashSet<>();
+        lhs.add(55);
+        lhs.add(99);
+        lhs.add(88);
+        lhs.add(12);
+        lhs.add(6);
+
+        System.out.println(lhs);
+    }
+    
+}
