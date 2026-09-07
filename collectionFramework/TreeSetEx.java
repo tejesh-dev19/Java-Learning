7b6d9bcfd4e21e05a30baeace7f3e26669190248
Tejesh Chintala <chintalatejesh2005@gmail.com>
2026-09-07 19:21:43 +0530

    add TreeSet example

---
+++ collectionFramework/TreeSetEx.java
@@ -0,0 +1,17 @@
+package collectionFramework;
+
+import java.util.TreeSet;
+
+public class TreeSetEx {
+    public static void main(String[] args) {
+        TreeSet<Integer> ts = new TreeSet<>();
+        ts.add(66);
+        ts.add(41);
+        ts.add(72);
+        ts.add(8);
+
+        System.out.println(ts);
+
+    }
+    
+}
