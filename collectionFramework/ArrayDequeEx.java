85f93a4094abfc9ca9d075b90393b761f04b0701
Tejesh Chintala <chintalatejesh2005@gmail.com>
2026-09-07 19:21:00 +0530

    add Arraydeque example

---
+++ collectionFramework/ArrayDequeEx.java
@@ -0,0 +1,18 @@
+package collectionFramework;
+
+import java.util.ArrayDeque;
+
+public class ArrayDequeEx {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> ad = new ArrayDeque<>();
+        ad.add(10);
+        ad.add(20);
+        ad.add(30);
+        ad.add(40);
+        ad.add(66);
+
+        System.out.println(ad);
+
+    }
+    
+}
