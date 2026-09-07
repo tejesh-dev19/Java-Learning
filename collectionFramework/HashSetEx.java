abeed8efef51cc5b5a77bacfee99cf1ee3f24a2c
Tejesh Chintala <chintalatejesh2005@gmail.com>
2026-09-07 19:21:58 +0530

    add HashSet example

---
+++ collectionFramework/HashSetEx.java
@@ -0,0 +1,16 @@
+package collectionFramework;
+
+import java.util.HashSet;
+
+public class HashSetEx {
+    public static void main(String[] args) {
+        HashSet hs = new HashSet<>();
+        hs.add(66);
+        hs.add(45);
+        hs.add(8);
+        hs.add(41);
+        hs.add(57);
+        System.out.println(hs);
+    }
+    
+}
