a97d78e982d59534760274ea1effa3314cb17890
Tejesh Chintala <chintalatejesh2005@gmail.com>
2026-09-08 18:11:28 +0530

    add ArrayList example

--- collectionFramework/ArrayListEx.java
+++ collectionFramework/ArrayListEx.java
@@ -1,22 +1,52 @@
 package collectionFramework;
 
 import java.util.ArrayList;
-import java.util.Collections;
+//import java.util.Collections;
+import java.util.Iterator;
+import java.util.ListIterator;
 
 public class ArrayListEx {
     public static void main(String[] args) {
-        ArrayList al = new ArrayList();
+        ArrayList<Integer> al = new ArrayList<>();
         al.add(10);
         al.add(20);
         al.add(30);
         al.add(66);
         al.add(0,58);
         al.add(99);
-        
 
-        Collections.sort(al);
+        for (int i = 0; i < al.size(); i++) {
+            Object o = al.get(i);
+            System.out.println(o);
+        }
 
-        System.out.println(al);
+        //for backward fetch the elements
+        ListIterator<Integer> itr = al.listIterator(al.size());
+        while(itr.hasPrevious()){
+            System.out.println(itr.previous());
+        }
+
+        System.out.println("*****************");
+        //for forward fetching
+        ListIterator<Integer> itr1 = al.listIterator();
+        while(itr1.hasNext()){
+            System.out.println(itr1.next());
+        }
+
+
+      //  System.out.println(al.get(0));
+        System.out.println("********************");
+
+        //normal iterator fetching
+        Iterator<Integer> itr2 = al.iterator();
+        while (itr2.hasNext()) {
+            System.out.println(itr2.next());
+        }
+
+
+        // Collections.sort(al);
+
+        // System.out.println(al);
     }
     
 }
