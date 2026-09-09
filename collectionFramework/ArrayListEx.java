0e9e8ea1d8c2818669562e15851bfda2371777b9
Tejesh Chintala <chintalatejesh2005@gmail.com>
2026-09-09 19:34:49 +0530

    add ArrayList example

--- collectionFramework/ArrayListEx.java
+++ collectionFramework/ArrayListEx.java
@@ -1,13 +1,16 @@
 package collectionFramework;
 
 import java.util.ArrayList;
+import java.util.Enumeration;
 //import java.util.Collections;
 import java.util.Iterator;
 import java.util.ListIterator;
+import java.util.Vector;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ArrayListEx {
     public static void main(String[] args) {
-        ArrayList<Integer> al = new ArrayList<>();
+        CopyOnWriteArrayList<Integer> al = new CopyOnWriteArrayList<>();
         al.add(10);
         al.add(20);
         al.add(30);
@@ -15,32 +18,43 @@ public class ArrayListEx {
         al.add(0,58);
         al.add(99);
 
-        for (int i = 0; i < al.size(); i++) {
-            Object o = al.get(i);
-            System.out.println(o);
-        }
+    //     for (int i = 0; i < al.size(); i++) {
+    //         Object o = al.get(i);
+    //         System.out.println(o);
+    //     }
 
-        //for backward fetch the elements
-        ListIterator<Integer> itr = al.listIterator(al.size());
-        while(itr.hasPrevious()){
-            System.out.println(itr.previous());
-        }
+    //     //for backward fetch the elements
+    //     ListIterator<Integer> itr = al.listIterator(al.size());
+    //     while(itr.hasPrevious()){
+    //         System.out.println(itr.previous());
+    //     }
 
-        System.out.println("*****************");
-        //for forward fetching
-        ListIterator<Integer> itr1 = al.listIterator();
-        while(itr1.hasNext()){
-            System.out.println(itr1.next());
-        }
+    //     System.out.println("*****************");
+    //     //for forward fetching
+    //     ListIterator<Integer> itr1 = al.listIterator();
+    //     while(itr1.hasNext()){
+    //         System.out.println(itr1.next());
+    //     }
 
 
-      //  System.out.println(al.get(0));
-        System.out.println("********************");
+    //   //  System.out.println(al.get(0));
+    //     System.out.println("********************");
 
         //normal iterator fetching
         Iterator<Integer> itr2 = al.iterator();
         while (itr2.hasNext()) {
             System.out.println(itr2.next());
+            al.add(20);
+        }
+        //before collection vector use 
+        Vector<Integer> v = new Vector<>();
+        v.add(20);
+        v.add(30);
+        v.add(40);
+
+        Enumeration<Integer> em=v.elements();
+        while (em.hasMoreElements()) {
+            System.out.println(em.nextElement());
         }
 
 
