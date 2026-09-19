1698005914e336e8c28e6a8e5f17059535ca2c34
Tejesh Chintala <chintalatejesh2005@gmail.com>
2026-09-19 22:56:16 +0530

    add Enum example

---
+++ enum_Anotation/LaunchEnum.java
@@ -0,0 +1,13 @@
+package enum_Anotation;
+
+enum Week{
+    MON,TUE,WED,THU,FRI,SAT,SUN
+}
+
+public class LaunchEnum {
+    public static void main(String[] args) {
+        System.out.println(Week.FRI);
+
+    }
+    
+}
